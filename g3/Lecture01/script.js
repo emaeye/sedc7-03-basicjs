c8e639ec9fbe922cdbf59fd087afd1a7f7c0d6b2
djovanov92 <djovanov92@gmail.com>
2018-11-07 16:26:14 +0100

    The source code from the exercises on the first class Basic JS

---
+++ g3/Lecture01/script.js
@@ -0,0 +1,21 @@
+let name = "Wekoslav";
+let lastName = "Stefanovski";
+
+let irish = "Denis O'Leary";
+
+let book = 'Дружината "Братско Стебло"';
+
+let age = 0x29;
+
+let fullName = name + " " + lastName;
+
+let formatedName = lastName + ", " + name + " (" + age + ")";  
+
+console.log(irish);
+console.log(book);
+console.log(fullName);
+console.log(formatedName);
+
+let fullerName = `${lastName}, ${name} (${age})`;
+
+console.log(fullerName);
---
+++ g3/Lecture01/toMeters.js
@@ -0,0 +1,6 @@
+let feet = 3456;
+let metersPerFoot = 0.3048;
+
+let meters = metersPerFoot * feet;
+
+console.log(`The length of ${feet} feet is equal to ${meters} meters`);
