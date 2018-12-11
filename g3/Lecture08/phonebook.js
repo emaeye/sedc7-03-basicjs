e2c667158082ef9ec9e2b87070d2da9e21f99035
djovanov92 <djovanov92@gmail.com>
2018-12-11 20:06:40 +0100

    working phonebook

--- g3/Lecture08/phonebook.js
+++ g3/Lecture08/phonebook.js
@@ -1,31 +1,6 @@
-let discard = document.getElementById("discard");
+/* DATA */
 
-discard.addEventListener("click", () => {
-  let firstName = document.getElementById("firstName");
-  let lastName = document.getElementById("lastName");
-  let email = document.getElementById("email");
-
-  firstName.value = "";
-  lastName.value = "";
-  email.value = "";
-});
-
-let save = document.getElementById("save");
-
-save.addEventListener("click", () => {
-  let firstName = document.getElementById("firstName").value;
-  let lastName = document.getElementById("lastName").value;
-  let email = document.getElementById("email").value;
-
-  console.log(firstName, lastName, email);
-  let contact = {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-  };
-  contacts.push(contact);
-  renderContacts(contacts);
-});
+let idCounter = 3;
 
 let contacts = [
   {
@@ -42,15 +17,67 @@ let contacts = [
   }
 ];
 
-let renderContacts = function(contacts) {
+let activeContact = null;
+
+/* EVENT HANDLERS */
+
+let discard = document.getElementById("discard");
+
+discard.addEventListener("click", () => {
+  // just remove the active contact
+  activeContact = null;
+  displayActiveContact();
+});
+
+let save = document.getElementById("save");
+
+save.addEventListener("click", () => {
+  // get the values from the inputs
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let email = document.getElementById("email").value;
+
+  if (activeContact) {
+    // we are updating a contact
+    activeContact.firstName = firstName;
+    activeContact.lastName = lastName;
+    activeContact.email = email;
+  } else {
+    // we are creating a new contact
+    let contact = {
+      id: idCounter,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+    idCounter +=1;
+    contacts.push(contact);
+    // set it as active
+    activeContact = contact;
+  }
+  displayContacts(contacts);
+});
+
+
+/* FUNCTIONS */
+
+/**
+ * Renders all given contacts
+ */
+let displayContacts = function(contacts) {
   let table = document.getElementById("result");
+  // clear previous rows, don't worry about performance
   table.innerText = "";
 
+  // render each contact in the table
   for (const contact of contacts) {
     renderContact(contact, table);
   }
 };
 
+/**
+ * Creates a table row for a single contact
+ */
 let renderContact = function(contact, table) {
   let row = table.insertRow();
 
@@ -70,17 +97,41 @@ let renderContact = function(contact, table) {
   let editBtn = document.createElement("button");
   editBtn.textContent = "edit";
   editBtn.addEventListener("click", () => {
-    console.log(`editing contact ${contact.id}`);
+    // just sets the active contact to the current contact
+    activeContact = contact;
+    displayActiveContact();
   });
 
   let deleteBtn = document.createElement("button");
   deleteBtn.textContent = "delete";
   deleteBtn.addEventListener("click", () => {
-    console.log(`deleting contact ${contact.id}`);
+    // find the index of the current contact and remove it
+    let index = contacts.indexOf(contact);
+    contacts.splice(index, 1);
+    displayContacts(contacts);
   });
 
   actionCell.appendChild(editBtn);
   actionCell.appendChild(deleteBtn);
 };
 
-renderContacts(contacts);
+/**
+ * Shows the active contact, or empty if there is no active contact
+ */
+let displayActiveContact = function (){
+  let firstName = document.getElementById("firstName");
+  let lastName = document.getElementById("lastName");
+  let email = document.getElementById("email");
+
+  if (activeContact) {
+    firstName.value = activeContact.firstName;
+    lastName.value = activeContact.lastName;
+    email.value = activeContact.email;
+  } else {
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+  }
+}
+
+displayContacts(contacts);
